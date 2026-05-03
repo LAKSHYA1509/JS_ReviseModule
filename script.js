1ac49207cda5ef2b30885c41b1a8a5b1aab6c863
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2026-05-03 07:19:47 +0000

    New code added

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // var, let, const - line-by-line comparison
 
-let constant = 10
-print(constant)
+// variable in es5 (Global existence rahegi poore code block mein )
+ var declare;
+ var declareAndInitialize = 12;
 
+ // let doesn't let you redeclaration but similar to var in other tasks
+
+ // const is like final in java
