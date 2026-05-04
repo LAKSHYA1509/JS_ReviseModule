74b2395d731ab5636f30213a98ad0fd8c0cf63e5
LAKSHYA BHARDWAJ <lakshyabhardwaj200315@gmail.com>
2026-05-04 10:18:51 +0000

    New code added

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // var, let, const - line-by-line comparison
 
 // variable in es5 (Global existence rahegi poore code block mein )
- var declare;
- var declareAndInitialize = 12;
+var declare;
+var declareAndInitialize = 12;
 
- // let doesn't let you redeclaration but similar to var in other tasks
+// let doesn't let you redeclaration but similar to var in other tasks
 
- // const is like final in java
- // Resassignment's done in both let and var, Redeclaration is done in var only.
+// const is like final in java
+// Resassignment's done in both let and var, Redeclaration is done in var only.
 
 
- // Temporal Dead Zone (TDZ) - It is the period during script execution when a variable declared with let or const is in scope but cannot be accessed because it has not yet been initialized.
+// Temporal Dead Zone (TDZ) - It is the period during script execution when a variable declared with let or const is in scope but cannot be accessed because it has not yet been initialized.
 
- // Isse already pata hai ki a aage hai but initialize nhi kar paa rha usse, Isliye print nhi kar paa rha
+// Isse already pata hai ki a aage hai but initialize nhi kar paa rha usse, Isliye print nhi kar paa rha
 //  console.log(a);
 //  let a = 12;
 
- // Hoisting -> ek variable ko jab js mein banate hai to wo variable do hisso mein toot jaata hai aur uska declare part upar chala jaata hai aur uska initilazation part neeche reh jaata hai
+// Hoisting -> ek variable ko jab js mein banate hai to wo variable do hisso mein toot jaata hai aur uska declare part upar chala jaata hai aur uska initilazation part neeche reh jaata hai
 
 
- // ---------------------------------------------------------
- // primitives, references -> inko copy karne par real copy nhi milegi but aapko reference milegi parent ki arrays, objects, function
+// ---------------------------------------------------------
+// primitives, references -> inko copy karne par real copy nhi milegi but aapko reference milegi parent ki arrays, objects, function
 
- // symbol -> unique immutable value
- // bigint -> addition of numbers bigger than Number.MAX_SAFE_INTEGER using n 
+// symbol -> unique immutable value
+// bigint -> addition of numbers bigger than Number.MAX_SAFE_INTEGER using n 
 
- // Dynamic typing -> js mein static typing nahi hai and yaha par hai dynamic typing jiska matlab hai aap data ko change kar sakte hai kyuki yaha par dynamic data types hai
+// Dynamic typing -> js mein static typing nahi hai and yaha par hai dynamic typing jiska matlab hai aap data ko change kar sakte hai kyuki yaha par dynamic data types hai
 
- // + to sirf concat kar sakta hai sirf isliye string and number ka aapas mein khela hobe
+// + to sirf concat kar sakta hai sirf isliye string and number ka aapas mein khela hobe
 
 // NaN is a failed number operation 
 
@@ -41,6 +41,8 @@
 // --------------------------------------------------------
 
 // Rock Paper Scissors -> I need simple 3 variables and user choice for it
+
+/*
 
 const user = prompt("Enter only in R, P, S")
 const choices = ['R', 'P', 'S']
@@ -62,3 +64,17 @@ function rps (user, robot) {
 }   
 
 rps(user, robot)
+
+*/
+
+// --------------------------------------------------------
+
+// For loop questions
+
+let val = prompt("all right you son of a bi...")
+if (val < 1) console.log("number smaller")
+
+    for(let i = 0; i <= val; i++) {
+        if(i%2==0) console.log(i + " is even")
+        else console.log(i + " is odd")
+    }
